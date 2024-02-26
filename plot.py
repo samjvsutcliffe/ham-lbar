94aa32de22c823808d0ecf7ce6bb5721151d8f1c
Sam Sutcliffe <sam.sutcliffe@.dur.ac.uk>
2024-02-26 09:46:55 +0000

    Updated plotting

--- plot.py
+++ plot.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import re
 
 from scipy import integrate
 def calculate_gf(disp,load):
@@ -10,12 +12,18 @@ data = pd.read_csv("load-disp.csv")
 
 print("GF experimental:",calculate_gf(1e0*data["disp"],100e-3*data["load"]))
 
-mpm = pd.read_csv("output/disp.csv")
 
-plt.plot(data["disp"],data["load"],label="Data")
-print("GF mpm:",calculate_gf(1e3*mpm["disp"],100e-3*mpm["load"]))
-plt.plot(1e3*mpm["disp"], 100e-3*mpm["load"],label="MPM")
-plt.xlabel("Displacement (mm)")
-plt.ylabel("Load (N)")
-plt.legend()
+regex = re.compile(r'output-.*')
+folders = list(filter(regex.search,os.listdir("./")))
+
+for i in folders:
+    print("loading folder: ",i)
+    plt.figure()
+    mpm = pd.read_csv("./{}/disp.csv".format(i))
+    plt.plot(data["disp"],data["load"],label="Data")
+    print("GF mpm:",calculate_gf(1e3*mpm["disp"],100e-3*mpm["load"]))
+    plt.plot(1e3*mpm["disp"],100e-3*mpm["load"],label="MPM")
+    plt.xlabel("Displacement (mm)")
+    plt.ylabel("Load (N)")
+    plt.legend()
 plt.show()
